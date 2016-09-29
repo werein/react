59e72865d6c34469dec29e831c1818c11e9d708b
Jiri Kolarik <jiri.kolarik@wereinhq.com>
2016-09-29 23:39:04 +0200

    Upgrade to latest deps and switch to jest testing framework

--- src/containers/__tests__/app.js
+++ src/containers/__tests__/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { store } from 'store';
 import { App } from '../app';
 
 describe('<App />', () => {
   it('renders <App /> container', () => {
-    const wrapper = shallow(<App store={store} />);
-    expect(wrapper.find('div')).to.have.length(1);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div').length).toBe(0);
+  });
+
+  it('renders <App /> container', () => {
+    const wrapper = shallow(<App loaded={true} />);
+    expect(wrapper.find('div').length).toBe(1);
   });
 });
